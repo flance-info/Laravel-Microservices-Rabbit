c5bd35872357dc9c910f53c8f9a109b7e5a45bef
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-08-02 18:08:49 +0500

    run migrateion and seeder

--- admin/database/factories/ProductFactory.php
+++ admin/database/factories/ProductFactory.php
@@ -2,21 +2,22 @@
 
 namespace Database\Factories;
 
+use App\Models\Models;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ProductFactory extends Factory
-{
+class ProductFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array
      */
     protected $model = Product::class;
-    
-    public function definition()
-    {
+
+    public function definition() {
         return [
-            //
+            'title' => $this->faker->text( 30 ),
+            'image' => $this->faker->imageUrl()
         ];
     }
 }
