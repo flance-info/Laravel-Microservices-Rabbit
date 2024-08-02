9df5b28de882eebe32fab4acc506880a8fe0f0ec
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-08-02 16:56:12 +0500

    addint factores and models

---
+++ admin/database/factories/ProductFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = Product::class;
+    
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
